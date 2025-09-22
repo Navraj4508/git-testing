0549f6ff888021b0db603ea6e783ba75dc393af9
Navraj4508 <166549018+Navraj4508@users.noreply.github.com>
2025-09-22 13:04:44 +0530

    Update HelloClass.java

--- src/Hello/HelloClass.java
+++ src/Hello/HelloClass.java
@@ -3,5 +3,7 @@ package Hello;
 public class HelloClass {
     public static void main(String[] args) {
         System.out.println("hello Bro");
+
+        System.out.println("Hello Git Hub");
     }
 }
